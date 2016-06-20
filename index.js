b382efdd84660c933d12e3b71366f37071f9da2d
Tuan <7u4n.pm@gmail.com>
2016-06-21 00:01:22 +0200

    first functional version of packtpub claimer

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
-var request = require('request');
+var config = require('./config.json');
+var request = require('request').defaults({
+    jar: true // enable cookie support, default is false
+});
+var cheerio = require('cheerio');
 
-request('https://www.packtpub.com/packt/offers/free-learning', function (error, response, body) {
+var packtpubBaseUrl = 'https://www.packtpub.com'
+var packtpubFreeEbookUrl = packtpubBaseUrl + '/packt/offers/free-learning';
+var userLoginForm;
+var freeEbookUrl;
+var formData = {
+	      email: config.packtpub.email,
+	      password: config.packtpub.password,
+	      op: 'Login',
+	      form_build_id: '',
+	      form_id: ''
+	    };
+
+request(packtpubFreeEbookUrl, function (error, response, body) {
   if (!error && response.statusCode == 200) {
-    console.log(body) // Show the HTML for the Google homepage.
+    $ = cheerio.load(body);
+    userLoginForm = $('#packt-user-login-form');
+    formData.form_build_id = userLoginForm.find('[name="form_build_id"]').val();
+    formData.form_id = userLoginForm.find('[name="form_id"]').val();
+    freeEbookUrl = packtpubBaseUrl + $('a.twelve-days-claim').attr("href");
+    console.log(freeEbookUrl);
+    request.post({url: packtpubFreeEbookUrl, formData: formData}, function (error, response, body) {
+      request(freeEbookUrl, function (error, response, body) {
+    	  console.log("DONE...");
+      });
+    });
   }
 })
